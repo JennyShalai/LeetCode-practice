99b26ed132ef5ea31b9340937cbea5a6b3997839
Jenny <jshalai@intrepid.io>
2022-04-30 08:16:44 -0400

    Update longest-common-prefix.py
    add second approach + description

--- longest-common-prefix.py
+++ longest-common-prefix.py
@@ -1,3 +1,12 @@
+# Write a function to find the longest common prefix string amongst an array of strings.
+# If there is no common prefix, return an empty string "".
+# Input: strs = ["flower","flow","flight"]
+# Output: "fl"
+
+# Two approaches, leetcode shows the same time and memory efficiency for several submissions
+
+# Take first string of a given array as a prefix
+# Go through array and compare prefix with each string and modify prefix accordingly
 def longestPrefix(strs):
   prefix = ''
   temp = ''
@@ -17,3 +26,18 @@ def longestPrefix(strs):
   return prefix
 
 print(longestPrefix(["flower","flow","flight"]))
+
+# For each char in the first string, go through a given array of strings and
+# compare with each char at the same char position
+def longestCommonPrefix(self, strs: List[str]) -> str:
+    prefix = ''
+    if len(strs) <= 1:
+        return strs[0]
+    index = 0
+    for char in strs[0]:
+        for s in strs:
+            if index >= len(s) or s[index] != char:
+                return prefix
+        prefix += char
+        index += 1
+    return prefix
