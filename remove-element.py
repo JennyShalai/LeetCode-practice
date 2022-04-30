c24599261d3288d5f8168548b2c1ec17f2d6d2b7
Jenny <jshalai@intrepid.io>
2022-04-30 09:00:08 -0400

    Update remove-element.py
    minor refactoring

--- remove-element.py
+++ remove-element.py
@@ -6,7 +6,7 @@
 
 def removeElement(nums, val):
     index = 0
-    for i in range(0, len(nums)):
+    for i in range(len(nums)):
       if nums[i] != val:
         nums[index] = nums[i]
         index += 1
