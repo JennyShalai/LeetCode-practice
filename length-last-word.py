8700a377aa677e1ce38fafb5f9048542467cd384
Jenny <jshalai@intrepid.io>
2022-05-17 11:40:15 -0400

    add efficient solution

--- length-last-word.py
+++ length-last-word.py
@@ -1,6 +1,16 @@
 # Length of Last Word: Given a string s consisting of some words separated by some number of spaces, return the length of the last word in the string.
 # A word is a maximal substring consisting of non-space characters only.
 
+def LengthOfLastWord(s):
+  index = len(s) - 1
+  k = 0
+  while s[index] == ' ':
+    index -= 1
+  while index >= 0 and s[index] != ' ':
+    k += 1
+    index -= 1
+  return k
+
 # with built-in functions
 def lengthOfLastWord2(s):
   s = s.strip()
@@ -8,7 +18,7 @@ def lengthOfLastWord2(s):
   return len(sArr[-1])
 
 # algorithmic approach
-def lengthOfLastWord1(s):
+def lengthOfLastWord3(s):
   s = s[::-1].strip()
   result = 0
   for char in s:
